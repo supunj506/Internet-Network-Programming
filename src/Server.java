6e3246ea65f0efb544b139c13016c399748edea1
supunj506 <supunj506@gmail.com>
2023-05-18 13:33:13 +0530

    ...created LocalSocket for Server

--- src/Server.java
+++ src/Server.java
@@ -8,12 +8,15 @@
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
 
         try {
             ServerSocket serverSocket=new ServerSocket(3000);
+            Socket accept = serverSocket.accept();
+
 
         } catch (IOException e) {
             e.printStackTrace();

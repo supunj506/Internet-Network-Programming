f77aad7727207a52b66f1e6c808472570ff99dfe
supunj506 <supunj506@gmail.com>
2023-05-20 23:47:13 +0530

    create Client form

---
+++ src/controller/ClientFormController.java
@@ -0,0 +1,12 @@
+/*
+ * @author : @MJ
+ * Date    : 5/20/2023
+ * Time    : 11:46 PM
+ * Project : INP
+ * Created by IntelliJ IDEA.
+ */
+
+package controller;
+
+public class ClientFormController {
+}

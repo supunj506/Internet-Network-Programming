78dd90e1735d756f8fd3706fa68748d2d4f13bed
supunj506 <supunj506@gmail.com>
2023-05-21 22:03:01 +0530

    create Server form

---
+++ src/controller/ServerFormController.java
@@ -0,0 +1,12 @@
+/*
+ * @author : @MJ
+ * Date    : 5/21/2023
+ * Time    : 10:02 PM
+ * Project : INP
+ * Created by IntelliJ IDEA.
+ */
+
+package controller;
+
+public class ServerFormController {
+}
